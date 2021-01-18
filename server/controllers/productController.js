7f8212afea27707d884a095878707a14eaffd9c0
ThisThato <iamkamzar@gmail.com>
2021-01-18 11:02:34 +0200

    product admin server functions

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -36,4 +36,63 @@ const deleteProduct = asynchandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct };
+//Update a product
+//PUT  /api/products/:id
+//private/Admin
+const updateProduct = asynchandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+    numReviews,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+    product.numReviews = numReviews;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new error("Product not found");
+  }
+});
+
+//Create a product
+//POST  /api/products
+//private/Admin
+const createProduct = asynchandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -4,17 +4,23 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  updateProduct,
+  createProduct,
 } from "../controllers/productController.js";
 const router = express.Router();
 
 //Fetch all products
 //GET  /api/products
 //public
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 
 //Fetch product by ID
 //GET  /api/product/:id
 //public
-router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
